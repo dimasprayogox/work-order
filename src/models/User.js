afece8b5d734595ba825ede6815834d635f06dd4
Aldeva Rizky <aldevarwpp@gmail.com>
2025-07-17 19:27:45 +0700

    refactor(user): use direct model imports for relationMappings
    - Replaced relative string paths with direct imports for related models
    - Models imported: WorkOrder, Issue, MaintenanceSchedule
    - Improves static analysis and IDE support

--- src/models/User.js
+++ src/models/User.js
@@ -1,48 +1,54 @@
 import { BaseModel } from './BaseModel.js';
+import { WorkOrder } from './WorkOrder.js';
+import { Issue } from './Issue.js';
+import { MaintenanceSchedule } from './MaintenanceSchedule.js';
 
 export class User extends BaseModel {
-  static get tableName() {
-    return 'users';
-  }
+    static get tableName() {
+        return 'users';
+    }
 
-  static get idColumn() {
-    return 'id';
-  }
+    static get idColumn() {
+        return 'id';
+    }
 
-  static get relationMappings() {
-    return {
-      createdWorkOrders: {
-        relation: BaseModel.HasManyRelation,
-        modelClass: './WorkOrder.js',
-        join: {
-          from: 'users.id',
-          to: 'work_orders.created_by_id',
-        },
-      },
-      assignedWorkOrders: {
-        relation: BaseModel.HasManyRelation,
-        modelClass: './WorkOrder.js',
-        join: {
-          from: 'users.id',
-          to: 'work_orders.assigned_to_id',
-        },
-      },
-      reportedIssues: {
-        relation: BaseModel.HasManyRelation,
-        modelClass: './Issue.js',
-        join: {
-          from: 'users.id',
-          to: 'issues.reported_by_id',
-        },
-      },
-      createdSchedules: {
-        relation: BaseModel.HasManyRelation,
-        modelClass: './MaintenanceSchedule.js',
-        join: {
-          from: 'users.id',
-          to: 'maintenance_schedules.created_by_id',
-        },
-      },
-    };
-  }
+    static get relationMappings() {
+        return {
+            createdWorkOrders: {
+                relation: BaseModel.HasManyRelation,
+                modelClass: WorkOrder,
+                join: {
+                    from: 'users.id',
+                    to: 'work_orders.created_by_id',
+                },
+            },
+
+            assignedWorkOrders: {
+                relation: BaseModel.HasManyRelation,
+                modelClass: WorkOrder,
+                join: {
+                    from: 'users.id',
+                    to: 'work_orders.assigned_to_id',
+                },
+            },
+
+            reportedIssues: {
+                relation: BaseModel.HasManyRelation,
+                modelClass: Issue,
+                join: {
+                    from: 'users.id',
+                    to: 'issues.reported_by_id',
+                },
+            },
+
+            createdSchedules: {
+                relation: BaseModel.HasManyRelation,
+                modelClass: MaintenanceSchedule,
+                join: {
+                    from: 'users.id',
+                    to: 'maintenance_schedules.created_by_id',
+                },
+            },
+        };
+    }
 }
