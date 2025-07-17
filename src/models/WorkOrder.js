944f6f317353848079a25d816738ee2f137c2e21
Aldeva Rizky <aldevarwpp@gmail.com>
2025-07-17 19:29:18 +0700

    refactor(work-order): use direct model imports in relationMappings
    - Replaced string paths with direct imports: Machine, User, Issue
    - Enhances static analysis, refactoring safety, and tooling support

--- src/models/WorkOrder.js
+++ src/models/WorkOrder.js
@@ -1,44 +1,50 @@
 import { BaseModel } from './BaseModel.js';
+import { Machine } from './Machine.js';
+import { User } from './User.js';
+import { Issue } from './Issue.js';
 
 export class WorkOrder extends BaseModel {
-  static get tableName() {
-    return 'work_orders';
-  }
+    static get tableName() {
+        return 'work_orders';
+    }
 
-  static get relationMappings() {
-    return {
-      machine: {
-        relation: BaseModel.BelongsToOneRelation,
-        modelClass: './Machine.js',
-        join: {
-          from: 'work_orders.machine_id',
-          to: 'machines.id',
-        },
-      },
-      assignedTo: {
-        relation: BaseModel.BelongsToOneRelation,
-        modelClass: './User.js',
-        join: {
-          from: 'work_orders.assigned_to_id',
-          to: 'users.id',
-        },
-      },
-      createdBy: {
-        relation: BaseModel.BelongsToOneRelation,
-        modelClass: './User.js',
-        join: {
-          from: 'work_orders.created_by_id',
-          to: 'users.id',
-        },
-      },
-      issues: {
-        relation: BaseModel.HasManyRelation,
-        modelClass: './Issue.js',
-        join: {
-          from: 'work_orders.id',
-          to: 'issues.work_order_id',
-        },
-      },
-    };
-  }
+    static get relationMappings() {
+        return {
+            machine: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: Machine,
+                join: {
+                    from: 'work_orders.machine_id',
+                    to: 'machines.id',
+                },
+            },
+
+            assignedTo: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: User,
+                join: {
+                    from: 'work_orders.assigned_to_id',
+                    to: 'users.id',
+                },
+            },
+
+            createdBy: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: User,
+                join: {
+                    from: 'work_orders.created_by_id',
+                    to: 'users.id',
+                },
+            },
+
+            issues: {
+                relation: BaseModel.HasManyRelation,
+                modelClass: Issue,
+                join: {
+                    from: 'work_orders.id',
+                    to: 'issues.work_order_id',
+                },
+            },
+        };
+    }
 }
