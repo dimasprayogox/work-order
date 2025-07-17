185f2f5fad40e5094e6f5650d6d210291aba63a4
Aldeva Rizky <aldevarwpp@gmail.com>
2025-07-17 19:24:20 +0700

    refactor(issue): update relationMappings to use direct model imports
    - Replaced string paths in relationMappings with direct imports of Machine, User, and WorkOrder models
    - Improves model resolution and avoids runtime issues in Objection.js

--- src/models/Issue.js
+++ src/models/Issue.js
@@ -1,36 +1,41 @@
 import { BaseModel } from './BaseModel.js';
+import { Machine } from './Machine.js';
+import { User } from './User.js';
+import { WorkOrder } from './WorkOrder.js';
 
 export class Issue extends BaseModel {
-  static get tableName() {
-    return 'issues';
-  }
+    static get tableName() {
+        return 'issues';
+    }
 
-  static get relationMappings() {
-    return {
-      machine: {
-        relation: BaseModel.BelongsToOneRelation,
-        modelClass: './Machine.js',
-        join: {
-          from: 'issues.machine_id',
-          to: 'machines.id',
-        },
-      },
-      reportedBy: {
-        relation: BaseModel.BelongsToOneRelation,
-        modelClass: './User.js',
-        join: {
-          from: 'issues.reported_by_id',
-          to: 'users.id',
-        },
-      },
-      workOrder: {
-        relation: BaseModel.BelongsToOneRelation,
-        modelClass: './WorkOrder.js',
-        join: {
-          from: 'issues.work_order_id',
-          to: 'work_orders.id',
-        },
-      },
-    };
-  }
+    static get relationMappings() {
+        return {
+            machine: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: Machine,
+                join: {
+                    from: 'issues.machine_id',
+                    to: 'machines.id',
+                },
+            },
+
+            reportedBy: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: User,
+                join: {
+                    from: 'issues.reported_by_id',
+                    to: 'users.id',
+                },
+            },
+
+            workOrder: {
+                relation: BaseModel.BelongsToOneRelation,
+                modelClass: WorkOrder,
+                join: {
+                    from: 'issues.work_order_id',
+                    to: 'work_orders.id',
+                },
+            },
+        };
+    }
 }
