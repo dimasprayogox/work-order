367f83d858aad1309b1475a2ee822d0cd1904e23
Toha Ikhsan <shadowex179@gmail.com>
2025-07-17 15:21:53 +0700

    feat: add role middleware

---
+++ src/middleware/role-middleware.js
@@ -0,0 +1,8 @@
+export const authorizeRole = (role) => {
+  return (req, res, next) => {
+    if (!req.user || req.user.role !== role) {
+      return res.status(403).json({ error: "Forbidden: Access denied" });
+    }
+    next();
+  };
+};
--- src/routes/admin/user.routes.js
+++ src/routes/admin/user.routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { UserController } from '../../controllers/admin/UserController.js';
 import { authMiddleware } from '../../middleware/auth-middleware.js';
+import { authorizeRole } from '../../middleware/role-middleware.js';
 
 const router = express.Router();
+
+// Hanya user yang sudah login dan punya role "admin"
 router.use(authMiddleware);
+router.use(authorizeRole('admin'));
 
 router.get('/', UserController.index);         // Get all users
 router.get('/:id', UserController.show);       // Get one user
