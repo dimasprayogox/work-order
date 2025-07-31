099cb00dbd1e9d24fc859bf8532588ae67cd2e47
Toha Ikhsan <shadowex179@gmail.com>
2025-07-31 12:10:30 +0700

    minor update

--- src/routes/employee/issue.route.js
+++ src/routes/employee/issue.route.js
@@ -11,9 +11,9 @@ router.use(authorizeRole('employee'));
 router.post("/", upload.single("photo"), IssueController.store);
 router.get("/", IssueController.getAll);
 router.get("/my-issues", IssueController.getMyIssues);
+router.post('/delete-many', IssueController.deleteMany)
 router.get("/:id", IssueController.getById);
 router.patch("/:id", upload.single("photo"), IssueController.update);
 router.delete("/:id", IssueController.delete);
-router.post('/delete-many', IssueController.deleteMany)
 
 export default router;
