7c02fe630bbcbf5e599f419d0a47e59d5bbe5a5c
Aldeva Rizky <aldevarwpp@gmail.com>
2025-08-01 10:38:09 +0700

    feat(IssueFormDialog): add user selection dropdown and fetch users API

---
+++ app/api/admin/issues/users/route.js
@@ -0,0 +1,28 @@
+import { Axios } from "../../../../utils/axios";
+import { API_ENDPOINTS } from "../../../api";
+import { NextResponse } from "next/server";
+import { isAxiosError } from "axios";
+
+/**
+ * Handler untuk mengambil semua users.
+ * @param {Request} request
+ */
+export const GET = async (request) => {
+    const token = request.cookies.get("authToken")?.value;
+    if (!token) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    try {
+        const response = await Axios.get(`${API_ENDPOINTS.ADMIN_ISSUES}/users`, {
+            headers: { Authorization: `Bearer ${token}` }
+        });
+        return NextResponse.json(response.data);
+    } catch (err) {
+        if (isAxiosError(err) && err.response) {
+            return NextResponse.json(err.response.data, { status: err.response.status });
+        }
+        console.error("[API USERS GET]", err);
+        return NextResponse.json({ message: "Gagal mengambil data users." }, { status: 500 });
+    }
+};
