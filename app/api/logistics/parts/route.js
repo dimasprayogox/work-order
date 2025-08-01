5a9a6eb74299161b25ac1fec4a176acc5e670bc7
Toha Ikhsan <shadowex179@gmail.com>
2025-08-01 15:48:00 +0700

    fix(logistic): cannot add data part

--- app/api/logistics/parts/route.js
+++ app/api/logistics/parts/route.js
@@ -1,4 +1,4 @@
-import { Axios } from "../../../utils/axios"; 
+import { Axios } from "../../../utils/axios";
 import { API_ENDPOINTS } from "../../api";
 import { NextResponse } from "next/server";
 import { isAxiosError } from "axios";
