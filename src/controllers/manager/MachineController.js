71e28c109e6860bc35dfc5bab79fc0ee7b05c011
dimasprayogox <dimasprayogo9c@gmail.com>
2025-08-22 09:08:29 +0700

    fix(manager): add condition get all when machine division same with user division

--- src/controllers/manager/MachineController.js
+++ src/controllers/manager/MachineController.js
@@ -1,12 +1,37 @@
-import { Machine } from '../../models/Machine.js';
+import { Machine } from "../../models/Machine.js";
+import { db } from "../../core/config/knex.js";
 
 export const MachineController = {
-    async index(req, res) {
-        try {
-            const machines = await Machine.query().withGraphFetched('category').orderBy('name', 'asc');
-            res.json({ success: true, data: machines });
-        } catch (err) {
-            res.status(500).json({ success: false, message: err.message });
-        }
-    },
+  async index(req, res) {
+    try {
+      const userId = req.user.userId;
+     
+      const userData = await db("users")
+        .leftJoin("divisions", "users.division_id", "divisions.id")
+        .where("users.id", userId)
+        .select(
+          "users.*",
+          "divisions.id as division_id",
+          "divisions.name as division_name"
+        )
+        .first();
+
+      const userDivisionId = userData?.division_id;
+    
+      let query = Machine.query()
+        .withGraphFetched("category")
+        .orderBy("name", "asc");
+     if (userDivisionId) {
+        query = query.where("division_id", userDivisionId);
+        } else {
+        return res.json({ success: true, data: [] });
+      }
+
+      const machines = await query;
+
+      res.json({ success: true, data: machines });
+    } catch (err) {
+      res.status(500).json({ success: false, message: err.message });
+    }
+  },
 };
